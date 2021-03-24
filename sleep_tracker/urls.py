f0b7e29b5cd163c27ea2e4c83742634d7390ae7c
Martin Yanchev <myanch200@gmail.com>
2021-03-24 16:59:49 +0000

    Adding sleep tracker to the navbar links

--- sleep_tracker/urls.py
+++ sleep_tracker/urls.py
@@ -5,6 +5,6 @@ from django.conf.urls.static import static
 from django.conf import settings
 app_name = "sleep_tracker"
 urlpatterns = [
-    path('', views.sleep_tracker, name='sleep_tracker'),
+    path('', views.sleep_tracker, name='dashboard'),
    
 ]
